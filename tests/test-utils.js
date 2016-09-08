2ba0c874cbc13d6c0636c258cb56a5fa3765602f
Mingliang Guo <mguo@ca.ibm.com>
2016-09-07 23:17:17 -0400

    minor update on tests

--- tests/test-utils.js
+++ tests/test-utils.js
@@ -1,14 +1,50 @@
-// tests/part1/cart-summary-test.js
 var chai = require('chai');
 var expect = chai.expect; // we are using the "expect" style of Chai
 var utils = require('./../lib/utils.js');
 
-describe('utils replace params for response file', function() {
+describe('utils provides a collection of utility methods', function() {
     it('replacePathParams() should replace params in url path with parameters in provided params', function() {
         var params = {
-            'cid': 'test_abc'
+            'cid': 'test_abc',
+            'pid': 'parent'
         };
         var value = utils.replacePathParams('/calendar/:cid/test', params);
         expect(value).to.equal('/calendar/test_abc/test');
+
+        value = utils.replacePathParams('/calendar/:cid', params);
+        expect(value).to.equal('/calendar/test_abc');
+
+        value = utils.replacePathParams('/calendar/:cid', params);
+        expect(value).to.equal('/calendar/test_abc');
+
+        value = utils.replacePathParams('/:pid/calendar/:cid', params);
+        expect(value).to.equal('/parent/calendar/test_abc');
+    });
+
+    it('mixin() should merge properties in the from object to the to object. And it should not overwrite unless overrite is set to true', function() {
+        var from = {
+            'cid': 'test_abc',
+            'pid': 'parent'
+        }, to = {};
+
+        utils.mixin(from, to);
+        expect(to.cid).to.equal('test_abc');
+
+        to = {'pid': 'pid'};
+        utils.mixin(from, to);
+        expect(to.cid).to.equal('test_abc');
+        expect(to.pid).to.equal('pid');
+
+        to = {'pid': 'pid'};
+        utils.mixin(from, to, true);
+        expect(to.cid).to.equal('test_abc');
+        expect(to.pid).to.equal('parent');
+
+        let toArray = [undefined, null, {}];
+        toArray.forEach(to => {
+            to = utils.mixin(from, to);
+            expect(to.cid).to.equal('test_abc');
+            expect(to.pid).to.equal('parent');
+        });
     });
 });
